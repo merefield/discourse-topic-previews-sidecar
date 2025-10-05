af62f5389dc705812b365341d502370926267dfa
merefield <merefield@gmail.com>
2025-10-05 15:32:23 +0100

    CHORE: bump patch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-topic-previews-sidecar
 # about: Sidecar Plugin to support Topic List Preview Theme Component
-# version: 6.2.0
+# version: 6.2.1
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-topic-previews
 
