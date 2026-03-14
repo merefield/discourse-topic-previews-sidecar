e29b7d409a6957f9b96b4db962c504a10a21eac9
merefield <merefield@gmail.com>
2026-03-14 18:54:19 +0000

    CHORE: bump plugin minor version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-topic-previews-sidecar
 # about: Sidecar Plugin to support Topic List Preview Theme Component
-# version: 6.2.1
+# version: 6.3.0
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-topic-previews
 
@@ -22,21 +22,13 @@ after_initialize do
     TopicViewSerializer.include(TopicPreviews::TopicViewSerializerExtension)
     ListHelper.prepend(TopicPreviews::ListHelperExtension)
     TopicList.prepend(TopicPreviews::TopicListExtension)
-    TopicListItemSerializer.include(
-      TopicPreviews::TopicListItemSerializerExtension
-    )
-    OptimizedImage.singleton_class.prepend(
-      TopicPreviews::OptimizedImageExtension
-    )
+    TopicListItemSerializer.include(TopicPreviews::TopicListItemSerializerExtension)
+    OptimizedImage.singleton_class.prepend(TopicPreviews::OptimizedImageExtension)
     CookedPostProcessor.prepend(TopicPreviews::CookedPostProcessorExtension)
     PostGuardian.prepend(TopicPreviews::PostGuardianExtension)
-    SuggestedTopicSerializer.include(
-      TopicPreviews::SuggestedTopicSerializerExtension
-    )
+    SuggestedTopicSerializer.include(TopicPreviews::SuggestedTopicSerializerExtension)
     if SiteSetting.topic_list_search_previews_enabled
-      SearchTopicListItemSerializer.prepend(
-        TopicPreviews::SearchTopicListItemSerializerExtension
-      )
+      SearchTopicListItemSerializer.prepend(TopicPreviews::SearchTopicListItemSerializerExtension)
     end
   end
 
@@ -44,10 +36,7 @@ after_initialize do
   register_topic_preloader_associations(:last_post)
   register_topic_preloader_associations(:image_upload)
 
-  User.register_custom_field_type(
-    "tlp_user_prefs_prefer_low_res_thumbnails",
-    :boolean
-  )
+  User.register_custom_field_type("tlp_user_prefs_prefer_low_res_thumbnails", :boolean)
   # register_topic_preloader_associations(:last_post_excerpt)
   Topic.register_custom_field_type("user_chosen_thumbnail_url", :string)
   Topic.register_custom_field_type("dominant_colour", :json)
@@ -75,9 +64,7 @@ after_initialize do
   if Topic.respond_to? :preloaded_custom_fields
     Topic.preloaded_custom_fields << "accepted_answer_post_id"
   end
-  if Topic.respond_to? :preloaded_custom_fields
-    Topic.preloaded_custom_fields << "dominant_colour"
-  end
+  Topic.preloaded_custom_fields << "dominant_colour" if Topic.respond_to? :preloaded_custom_fields
   # DiscourseEvent.on(:accepted_solution) do |post|
   #   if post.image_url && SiteSetting.topic_list_previews_enabled
   #     ListHelper.create_topic_thumbnails(post, post.image_url)[:id]
@@ -91,7 +78,7 @@ after_initialize do
     tc.record_change(
       "user_chosen_thumbnail_url",
       tc.topic.custom_fields["user_thumbnail_choice"],
-      tf
+      tf,
     )
     tc.topic.custom_fields["user_chosen_thumbnail_url"] = tf
   end
@@ -100,18 +87,14 @@ after_initialize do
     tc.topic.image_upload_id = tf
   end
   PostRevisor.track_topic_field("force_latest_post_nav".to_sym) do |tc, tf|
-    tc.record_change(
-      "force_latest_post_nav",
-      tc.topic.custom_fields["force_latest_post_nav"],
-      tf
-    )
+    tc.record_change("force_latest_post_nav", tc.topic.custom_fields["force_latest_post_nav"], tf)
     tc.topic.custom_fields["force_latest_post_nav"] = tf
   end
   PostRevisor.track_topic_field("show_latest_post_excerpt".to_sym) do |tc, tf|
     tc.record_change(
       "show_latest_post_excerpt",
       tc.topic.custom_fields["show_latest_post_excerpt"],
-      tf
+      tf,
     )
     tc.topic.custom_fields["show_latest_post_excerpt"] = tf
   end
